fff4d5032e26d977e98ba716d9ee83bf069b984b
Yuliia <ciceron131313@gmail.com>
2026-01-21 00:52:24 +0200

    h

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/Header/Header";
+import Footer from "@/components/Footer/Footer";
+import TanStackProvider from "@/components/TanStackProvider/TanStackProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -18,14 +22,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+  children
+}: Readonly<{ children: React.ReactNode; }>) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
+        <TanStackProvider>
+          <Header />
+          <main>{children}</main>
+          <Footer/>
+        </TanStackProvider>
       </body>
     </html>
   );
