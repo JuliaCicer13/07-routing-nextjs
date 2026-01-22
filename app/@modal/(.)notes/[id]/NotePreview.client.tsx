229006a23e6d600eca09a32dba527a7e4885bdd2
Yuliia <ciceron131313@gmail.com>
2026-01-22 23:52:35 +0200

    h

--- app/@modal/(.)notes/[id]/NotePreview.client.tsx
+++ app/@modal/(.)notes/[id]/NotePreview.client.tsx
@@ -1,22 +1,32 @@
 'use client';
 
-import { getSingleNote } from '@/lib/api';
+import { fetchNoteById } from '@/lib/api';
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from 'next/navigation';
 import Modal from '@/components/Modal/Modal';
 
-type Props = {
-  params: Promise<{ id: string }>;
-};
 
-const NotePreview = async ({ params }: Props) => {
-  const { id } = await params;
-  const note = await getSingleNote(id);
+const NotePreview =  () => {
+  const { id } = useParams<{ id: string }>();
+
+   const { data: note, isLoading, error } = useQuery({
+    queryKey: ["note", id],
+    queryFn: () => fetchNoteById(id),
+    refetchOnMount: false,
+  });
+   if (isLoading) return <p>Loading...</p>;
+
+  if (error || !note) return <p>Some error..</p>;
+
+  const formattedDate = note.updatedAt
+    ? `Updated at: ${note.updatedAt}`
+    : `Created at: ${note.createdAt}`;
 
   return (
     <Modal onClick={close}>
       <h2>{note.title}</h2>
       <p>{note.content}</p>
+        <p>{formattedDate}</p>
     </Modal>
   );
 };
