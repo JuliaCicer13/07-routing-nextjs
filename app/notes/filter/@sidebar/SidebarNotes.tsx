17545dd96ddae4da82de9133c1e39cd7a940f7b5
Yuliia <ciceron131313@gmail.com>
2026-02-06 22:36:28 +0200

    j

--- app/notes/filter/@sidebar/SidebarNotes.tsx
+++ app/notes/filter/@sidebar/SidebarNotes.tsx
@@ -1,11 +1,12 @@
 import Link from 'next/link';
 import { TAGS } from '@/constants/tags';
+import css from '../@sidebar/SidebarNotes.module.css';
 
 const SidebarNotes = async () => {
   return (
-    <ul>
-      <li>
-        <Link href={`/notes/filter/all`}>All notes</Link>
+    <ul className={css.menuList}>
+      <li className={css.menuItem }>
+        <Link className={css.menuLink} href={`/notes/filter/all`}>All notes</Link>
       </li>
       {TAGS.map((tag) => (
         <li key={tag}>
